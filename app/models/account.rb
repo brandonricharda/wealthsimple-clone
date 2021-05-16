e09c0cd4a34cdbed436e9eaa6ba5e32eead232bc
brandonricharda <brandonricharda@gmail.com>
2021-05-15 22:51:00 -0400

    Adds validation, association, and update_available_balance method

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,6 +1,13 @@
 class Account < ApplicationRecord
     validates :name, presence: true
+    validates :user_id, presence: true
 
     belongs_to :user
+    has_many :holdings
     has_one :portfolio 
+
+    def update_available_balance(amount)
+        new_balance = amount > 0 ? self.available_balance - amount : self.available_balance + amount
+        self.update(:available_balance => new_balance)
+    end
 end
