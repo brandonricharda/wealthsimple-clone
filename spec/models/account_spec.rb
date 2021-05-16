e972949713fee084e715dbb315e9398d69430cfb
brandonricharda <brandonricharda@gmail.com>
2021-05-15 22:54:24 -0400

    Adds test for update_balance method

--- spec/models/account_spec.rb
+++ spec/models/account_spec.rb
@@ -2,6 +2,8 @@ require 'rails_helper'
 
 RSpec.describe Account, :type => :model do
 
+    after(:all) { User.destroy_all }
+
     describe "#new" do
 
         context "when called without name" do
@@ -14,23 +16,20 @@ RSpec.describe Account, :type => :model do
 
     describe "#create" do
 
+        let(:user) {
+            User.create(
+                :name => ENV["valid_name"],
+                :email => ENV["valid_email"],
+                :password => ENV["password"]
+            )
+        }
+
         context "when called with valid input" do
-            it "sets balance to 0.0 by default" do
+            it "sets balance to 0 by default" do
                 expect(Account.create(
-                  :name => ENV["account_name"]
-                ).balance).to eql 0.0
-            end
-        end
-
-    end
-
-    describe ".user" do
-
-        context "when called on non-associated account" do
-            it "returns nil" do
-                expect(Account.create(
-                    :name => ENV["account_name"]
-                ).user).to eql nil
+                  :name => ENV["account_name"],
+                  :user_id => user.id
+                ).available_balance).to eql 0
             end
         end
 
@@ -43,6 +42,32 @@ RSpec.describe Account, :type => :model do
                 expect(Account.create(
                     :name => ENV["account_name"]
                 ).portfolio).to eql nil
+            end
+        end
+
+    end
+
+    describe ".update_balance" do
+
+        let(:user) {
+            User.create(
+                :name => ENV["valid_name"],
+                :email => ENV["valid_email"],
+                :password => ENV["password"]
+            )
+        }
+
+        let(:account) {
+            user.accounts.create(
+                :name => ENV["account_name"],
+                :user_id => user.id ,
+                :available_balance => 1000000
+            )
+        }
+
+        context "when called after transaction" do
+            it "updates available balance" do
+                expect { account.update_available_balance(-10000) }.to change { account.available_balance }.by(-10000)
             end
         end
 
