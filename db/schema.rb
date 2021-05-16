f3b646877ac220f5ab22e13593773c8e0653c1f4
brandonricharda <brandonricharda@gmail.com>
2021-05-15 23:25:05 -0400

    Adds account_id to portfolio

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_16_023741) do
+ActiveRecord::Schema.define(version: 2021_05_16_032242) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -46,6 +46,8 @@ ActiveRecord::Schema.define(version: 2021_05_16_023741) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "risk_tolerance"
+    t.bigint "account_id"
+    t.index ["account_id"], name: "index_portfolios_on_account_id"
   end
 
   create_table "users", force: :cascade do |t|
