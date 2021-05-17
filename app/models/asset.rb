0369a0291c2ff566cf20d79af58a43566293f6cf
brandonricharda <brandonricharda@gmail.com>
2021-05-16 20:19:14 -0400

    Updates tests to be less false positive-prone

--- app/models/asset.rb
+++ app/models/asset.rb
@@ -1,5 +1,5 @@
 class Asset < ApplicationRecord
     validates :ticker, presence: true
-    validates :price, presence: true, numericality: { greater_than_or_equal_to: 0 }
+    validates :price, presence: true, numericality: { greater_than_or_equal_to: 1 }
     validates :riskiness, presence: true, numericality: { greater_than_or_equal_to: 1, less_than_or_equal_to: 5 }
 end
--- spec/models/asset_spec.rb
+++ spec/models/asset_spec.rb
@@ -5,90 +5,249 @@ RSpec.describe Asset, :type => :model do
     describe "#create" do
 
         context "when called without params" do
+
+            let(:asset) { Asset.create }
+
             it "doesn't create record" do
-                expect { Asset.create }.to_not change { Asset.count }
+                expect { asset }.to_not change { Asset.count }
             end
+
+            it "returns five errors" do
+                expect(asset.errors.count).to eql 5
+            end
+
+            it "returns ticker blank error" do
+                expect(asset.errors[:ticker].first).to eql "can't be blank"
+            end
+
+            it "returns price blank error" do
+                expect(asset.errors[:price].first).to eql "can't be blank"
+            end
+
+            it "returns price not number error" do
+                expect(asset.errors[:price].last).to eql "is not a number"
+            end
+
+            it "returns riskiness blank error" do
+                expect(asset.errors[:riskiness].first).to eql "can't be blank"
+            end
+
+            it "returns riskiness not number error" do
+                expect(asset.errors[:riskiness].last).to eql "is not a number"
+            end
+
         end
 
         context "when called with just price" do
+
+            let(:asset) { Asset.create(:price => 100) }
+
             it "doesn't create record" do
-                expect { Asset.create(
-                    :price => 100
-                ) }.to_not change { Asset.count }
+                expect { asset }.to_not change { Asset.count }
             end
+
+            it "returns three errors" do
+                expect(asset.errors.count).to eql 3
+            end
+
+            it "returns ticker blank error" do
+                expect(asset.errors[:ticker].first).to eql "can't be blank"
+            end
+
+            it "returns riskiness blank error" do
+                expect(asset.errors[:riskiness].first).to eql "can't be blank"
+            end
+
+            it "returns riskiness not number error" do
+                expect(asset.errors[:riskiness].last).to eql "is not a number"
+            end
+
         end
 
         context "when called with just riskiness" do
+
+            let(:asset) { Asset.create(:riskiness => 5) }
+
             it "doesn't create record" do
-                expect { Asset.create(
-                    :riskiness => 5
-                ) }.to_not change { Asset.count }
+                expect { asset }.to_not change { Asset.count }
             end
+
+            it "returns three errors" do
+                expect(asset.errors.count).to eql 3
+            end
+
+            it "returns ticker blank error" do
+                expect(asset.errors[:ticker].first).to eql "can't be blank"
+            end
+
+            it "returns price blank error" do
+                expect(asset.errors[:price].first).to eql "can't be blank"
+            end
+
+            it "returns price not number error" do
+                expect(asset.errors[:price].last).to eql "is not a number"
+            end
+
         end
 
         context "when called with just ticker" do
+
+            let(:asset) { Asset.create(:ticker => "AAPL") }
+            
             it "doesn't create record" do
-                expect { Asset.create(
-                    :ticker => "AAPL"
-                ) }.to_not change { Asset.count }
+                expect { asset }.to_not change { Asset.count }
             end
+
+            it "returns four errors" do
+                expect(asset.errors.count).to eql 4
+            end
+
+            it "returns no price error" do
+                expect(asset.errors[:price].first).to eql "can't be blank"
+            end
+
+            it "returns price not number error" do
+                expect(asset.errors[:price].last).to eql "is not a number"
+            end
+
+            it "returns riskiness blank error" do
+                expect(asset.errors[:riskiness].first).to eql "can't be blank"
+            end
+
+            it "returns riskiness not number error" do
+                expect(asset.errors[:riskiness].last).to eql "is not a number"
+            end
+
         end
 
         context "when called with just price and riskiness" do
+
+            let(:asset) { Asset.create(:price => 100, :riskiness => 5) }
+
             it "doesn't create record" do
-                expect { Asset.create(
-                    :price => 100,
-                    :riskiness => 5
-                ) }.to_not change { Asset.count }
+                expect { asset }.to_not change { Asset.count }
             end
+
+            it "returns one error" do
+                expect(asset.errors.count).to eql 1
+            end
+
+            it "returns ticker blank error" do
+                expect(asset.errors[:ticker].first).to eql "can't be blank"
+            end
+
         end
 
         context "when called with just price and ticker" do
+
+            let(:asset) { Asset.create(:price => 100, :ticker => "AAPL") }
+
             it "doesn't create record" do
-                expect { Asset.create(
-                    :price => 100,
-                    :ticker => "AAPL"
-                ) }
+                expect { asset }.to_not change { Asset.count }
             end
+
+            it "returns two errors" do
+                expect(asset.errors.count).to eql 2
+            end
+
+            it "returns riskiness blank error" do
+                expect(asset.errors[:riskiness].first).to eql "can't be blank"
+            end
+
+            it "returns riskiness not number error" do
+                expect(asset.errors[:riskiness].last).to eql "is not a number"
+            end
+
         end
 
         context "when called with just riskiness and ticker" do
+
+            let(:asset) { Asset.create(:riskiness => 5, :ticker => "AAPL") }
+
             it "doesn't create record" do
-                expect { Asset.create(
-                    :riskiness => 5,
-                    :ticker => "AAPL"
-                ) }.to_not change { Asset.count }
+                expect { asset }.to_not change { Asset.count }
             end
+
+            it "returns two errors" do
+                expect(asset.errors.count).to eql 2
+            end
+
+            it "returns no price error" do
+                expect(asset.errors[:price].first).to eql "can't be blank"
+            end
+
+            it "returns price not number error" do
+                expect(asset.errors[:price].last).to eql "is not a number"
+            end
+
         end
 
         context "when called with full params" do
+
+            let(:asset) { Asset.create(:ticker => "AAPL", :price => 100, :riskiness => 5) }
+
             it "creates record" do
-                expect { Asset.create(
-                    :ticker => "AAPL",
-                    :price => 100,
-                    :riskiness => 5
-                ) }.to change { Asset.count }.by 1
+                expect { asset }.to change { Asset.count }
             end
+
+            it "returns no errors" do
+                expect(asset.errors.count).to eql 0
+            end
+
         end
 
         context "when called with riskiness > 5" do
+
+            let(:asset) { Asset.create(:ticker => "AAPL", :price => 100, :riskiness => 6) }
+
             it "doesn't create record" do
-                expect { Asset.create(
-                    :ticker => "AAPL",
-                    :price => 100,
-                    :riskiness => 6
-                ) }.to_not change { Asset.count }
+                expect { asset }.to_not change { Asset.count }
             end
+
+            it "returns one error" do
+                expect(asset.errors.count).to eql 1
+            end
+
+            it "returns riskiness outside range" do
+                expect(asset.errors[:riskiness].first).to eql "must be less than or equal to 5"
+            end
+
         end
 
         context "when called with riskiness < 1" do
+
+            let(:asset) { Asset.create(:ticker => "AAPL", :price => 100, :riskiness => 0) }
+
             it "doesn't create record" do
-                expect { Asset.create(
-                    :ticker => "AAPL",
-                    :price => 100,
-                    :riskiness => 0
-                ) }.to_not change { Asset.count }
+                expect { asset }.to_not change { Asset.count }
             end
+
+            it "returns one error" do
+                expect(asset.errors.count).to eql 1
+            end
+
+            it "returns riskiness outside range" do
+                expect(asset.errors[:riskiness].first).to eql "must be greater than or equal to 1"
+            end
+
+        end
+
+        context "when called with price of 0" do
+
+            let(:asset) { Asset.create(:ticker => "AAPL", :price => 0, :riskiness => 5) }
+
+            it "doesn't create record" do
+                expect { asset }.to_not change { Asset.count }
+            end
+
+            it "returns one error" do
+                expect(asset.errors.count).to eql 1
+            end
+
+            it "returns price below 1 error" do
+                expect(asset.errors[:price].first).to eql "must be greater than or equal to 1"
+            end
+
         end
 
     end
