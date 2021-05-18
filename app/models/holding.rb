211790cc48bc60abb247a50152e68f076318d649
brandonricharda <brandonricharda@gmail.com>
2021-05-18 09:41:49 -0400

    Add update_account_balances method to holding
    When a new holding is created, it will now automatically update the
    asset_balance and available_balance on the associated account using a
    callback and model method

--- app/models/holding.rb
+++ app/models/holding.rb
@@ -4,7 +4,15 @@ class Holding < ApplicationRecord
     belongs_to :asset
     belongs_to :account
 
+    after_create :update_account_balances
+
     def valuation
         self.units * asset.price
+    end
+
+    def update_account_balances
+        change = self.valuation
+        self.account.update_available_balance(-change)
+        self.account.update_asset_balance(change)
     end
 end
