f17390cc1e749933fd80ad348c18df74a263827e
brandonricharda <brandonricharda@gmail.com>
2021-05-17 22:09:38 -0400

    Add withdraw and deposit methods to account
    These methods will be part of the mechanism through which transactions
    (which will be a separate model) are finalized/reflected in the
    account model

--- app/models/account.rb
+++ app/models/account.rb
@@ -4,6 +4,18 @@ class Account < ApplicationRecord
     belongs_to :user
     has_many :holdings
 
+    def deposit(amount)
+        return false, "Deposit amount cannot be negative" if amount < 0
+        return false, "Deposit amount cannot be zero dollars" if amount == 0
+        self.update_available_balance(amount)
+    end
+
+    def withdraw(amount)
+        return false, "Please format withdrawal amount as negative number" if amount > 0
+        return false, "Withdrawal amount cannot be zero dollars" if amount == 0
+        self.update_available_balance(amount)
+    end
+
     def update_available_balance(amount)
         new_balance = self.available_balance + amount
         return false, "Transaction exceeds available balance" if new_balance < 0
