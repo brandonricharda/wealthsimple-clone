2d8a2fbc58771d76d6a0db08fb0408f40e320e8d
brandonricharda <brandonricharda@gmail.com>
2021-05-17 20:07:42 -0400

    remove time_horizon from user and create risk_profile table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_16_165423) do
+ActiveRecord::Schema.define(version: 2021_05_18_000215) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,6 +43,16 @@ ActiveRecord::Schema.define(version: 2021_05_16_165423) do
     t.index ["asset_id"], name: "index_holdings_on_asset_id"
   end
 
+  create_table "risk_profiles", force: :cascade do |t|
+    t.integer "time_horizon"
+    t.integer "risk_aversion"
+    t.integer "job_stability"
+    t.bigint "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_risk_profiles_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -50,7 +60,6 @@ ActiveRecord::Schema.define(version: 2021_05_16_165423) do
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
     t.integer "risk_tolerance", default: 0
-    t.integer "time_horizon", default: 10
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "name"
