589087a6b4ba99bfe36c19b3847f8d8a84acfb49
brandonricharda <brandonricharda@gmail.com>
2021-05-23 15:20:44 -0400

    Make sign-in form redirect to correct page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::Base
+    def after_sign_in_path_for(resource)
+        if resource.class == User
+            user_url(resource.id)
+        end
+    end
 end
