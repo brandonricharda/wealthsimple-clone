8d8e74cf5fff92ffe32e8aae7c47f1e3426e0594
brandonricharda <brandonricharda@gmail.com>
2021-05-23 22:47:10 -0400

    Remove Asset Sale functionality of transaction
    Having "Asset Sale" and "Withdrawal" are redundant since withdrawals
    shouldn't go to an account's available_balance but rather simply
    be withdrawn from the platform altogether

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,7 +1,7 @@
 class Transaction < ApplicationRecord
     validates :amount, presence: true
     validates :description, presence: true
-    validates_inclusion_of :description, :in => ["Investment", "Deposit", "Withdrawal", "Asset Sale"]
+    validates_inclusion_of :description, :in => ["Investment", "Deposit", "Withdrawal"]
     validate :sufficient_funds
     validate :at_least_one_share
 
@@ -11,12 +11,8 @@ class Transaction < ApplicationRecord
 
     def update_account(leftover)
         if self.description == "Withdrawal"
-            new_available_balance = self.account.available_balance - self.amount
-            self.account.update(:available_balance => new_available_balance)
-        elsif self.description == "Asset Sale"
             new_asset_balance = self.account.asset_balance - (self.amount - leftover)
-            new_available_balance = self.account.available_balance + (self.amount - leftover)
-            self.account.update(:asset_balance => new_asset_balance, :available_balance => new_available_balance)
+            self.account.update(:asset_balance => new_asset_balance)
         elsif self.description == "Investment"
             new_asset_balance = self.account.asset_balance + (self.amount - leftover)
             new_available_balance = self.account.available_balance - (self.amount - leftover)
@@ -29,12 +25,12 @@ class Transaction < ApplicationRecord
     end
 
     def update_holding
-        if self.description == "Asset Sale" || self.description == "Investment"
+        if self.description == "Withdrawal" || self.description == "Investment"
             asset_price = self.account.holding.asset.price
             transaction_amount = self.amount
             units_transacted = transaction_amount / asset_price
             leftover = transaction_amount % asset_price
-            new_units = self.description == "Asset Sale" ? self.account.holding.units - units_transacted : self.account.holding.units + units_transacted
+            new_units = self.description == "Withdrawal" ? self.account.holding.units - units_transacted : self.account.holding.units + units_transacted
             self.account.holding.update(:units => new_units)
         end
         update_account(leftover)
@@ -43,12 +39,12 @@ class Transaction < ApplicationRecord
     private
 
     def sufficient_funds
-        if self.description == "Withdrawal" || self.description == "Investment"
+        if self.description == "Withdrawal"
+            new_asset_balance = self.account.asset_balance - self.amount
+            errors.add(:amount, "exceeds asset balance") if new_asset_balance < 0
+        elsif self.description == "Investment"
             new_available_balance = self.account.available_balance - self.amount
             errors.add(:amount, "exceeds available balance") if new_available_balance < 0
-        elsif self.description == "Asset Sale"
-            new_available_balance = self.account.asset_balance - self.amount
-            errors.add(:amount, "exceeds asset balance") if new_available_balance < 0
         end
     end
 
--- spec/models/transaction_spec.rb
+++ spec/models/transaction_spec.rb
@@ -65,9 +65,9 @@ RSpec.describe Transaction, :type => :model do
                 expect { account.transactions.create(:amount => 1000, :description => "Withdrawal") }.to change { Transaction.count }.by 1
             end
 
-            it "accepts Asset Sale" do
-                expect { account.transactions.create(:amount => 1000, :description => "Asset Sale") }.to change { Transaction.count }.by 1
-            end
+            # it "accepts Asset Sale" do
+            #     expect { account.transactions.create(:amount => 1000, :description => "Asset Sale") }.to change { Transaction.count }.by 1
+            # end
 
             it "rejects anything else" do
                 expect { account.transactions.create(:amount => 1000, :description => "anything else") }.to_not change { Transaction.count }
@@ -84,20 +84,6 @@ RSpec.describe Transaction, :type => :model do
             end
 
             it "returns excessive withdrawal error" do
-                expect(transaction.errors[:amount].first).to eql "exceeds available balance"
-            end
-
-        end
-
-        context "when called with excessive Asset Sale" do
-
-            let(:transaction) { account.transactions.create(:amount => 100000, :description => "Asset Sale") }
-
-            it "doesn't create record" do
-                expect { transaction }.to_not change { Transaction.count }
-            end
-
-            it "returns excessive asset sale error" do
                 expect(transaction.errors[:amount].first).to eql "exceeds asset balance"
             end
 
@@ -111,30 +97,12 @@ RSpec.describe Transaction, :type => :model do
                 expect { transaction }.to change { Transaction.count }.by 1
             end
 
-            it "adjusts available balance" do
-                expect { transaction }.to change { account.available_balance }.by -500
-            end
-
-        end
-
-        context "when called with allowable Asset Sale" do
-
-            let(:transaction) { account.transactions.create(:amount => 500, :description => "Asset Sale") }
-
-            it "creates record" do
-                expect { transaction }.to change { Transaction.count }.by 1
-            end
-
             it "adjusts asset balance" do
                 expect { transaction }.to change { account.asset_balance }.by -500
             end
 
-            it "adjusts available balance" do
-                expect { transaction }.to change { account.available_balance }.by 500
-            end
-
-            it "adjusts holding balance" do
-                expect { transaction }.to change { account.holding.units }.by -5
+            it "doesn't change available balance" do
+                expect { transaction }.to_not change { account.available_balance }
             end
 
         end
@@ -171,42 +139,6 @@ RSpec.describe Transaction, :type => :model do
 
             it "returns transaction < asset price error" do
                 expect(transaction.errors[:amount].first).to eql "transaction must be worth at least one share"
-            end
-
-        end
-
-        context "when asset sale amount < asset price" do
-            
-            let(:transaction) { account.transactions.create(:amount => 80, :description => "Asset Sale") }
-
-            it "doesn't create record" do
-                expect { transaction }.to_not change { Transaction.count }
-            end
-
-            it "returns transaction < asset price error" do
-                expect(transaction.errors[:amount].first).to eql "transaction must be worth at least one share"
-            end
-
-        end
-
-        context "when asset sale amount % price != 0" do
-
-            let(:transaction) { account.transactions.create(:amount => 110, :description => "Asset Sale") }
-
-            it "creates record" do
-                expect { transaction }.to change { Transaction.count }
-            end
-
-            it "transacts whatever shares it can" do
-                expect { transaction }.to change { account.holding.units }.by -1
-            end
-
-            it "keeps remainder in assets" do
-                expect { transaction }.to change { account.asset_balance }.by -100
-            end
-
-            it "doesn't send remainder to cash" do
-                expect { transaction }.to change { account.available_balance }.by 100
             end
 
         end
