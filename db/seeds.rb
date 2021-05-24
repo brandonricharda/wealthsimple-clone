72658d203243dbe65fa7b95125d84f0fadd57118
brandonricharda <brandonricharda@gmail.com>
2021-05-23 22:26:07 -0400

    Add five assets to seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Asset.create(:ticker => "CASH", :price => 1, :riskiness => 1)
+Asset.create(:ticker => "BONDS", :price => 1, :riskiness => 2)
+Asset.create(:ticker => "GOLD", :price => 1, :riskiness => 3)
+Asset.create(:ticker => "VTI", :price => 1, :riskiness => 4)
+Asset.create(:ticker => "SPY", :price => 1, :riskiness => 5)
